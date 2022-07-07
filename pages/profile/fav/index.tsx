95c8d32c2342f681cac135242e39aec919e12d69
plrs9816 <61955474+plrs9816@users.noreply.github.com>
2022-07-08 07:35:05 +0900

    refactor: barrel

--- pages/profile/fav/index.tsx
+++ pages/profile/fav/index.tsx
@@ -1,7 +1,7 @@
 import { Section } from "common/atoms";
 import { CustomNextPage } from "types";
 import { createPageLayout } from "components";
-import FavListView from "views/profile.fav/FavListView";
+import { FavListView } from "views/profile.fav";
 
 const Fav: CustomNextPage = () => {
   return (
---
+++ views/profile.fav/index.ts
@@ -0,0 +1,1 @@
+export { default as FavListView } from "./FavListView";
