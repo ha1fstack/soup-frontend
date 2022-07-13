a1540dcd07cd703a5aa35cfc0c2c5fc00ddf51f9
starlesssss <61955474+plrs9816@users.noreply.github.com>
2022-07-13 23:14:15 +0900

    fix: gtag id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -180,7 +180,7 @@ export default function App({
       {!isDevEnv && (
         <>
           <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_GA_MEASUREMENT_ID}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
             strategy="afterInteractive"
           />
           <Script id="google-analytics" strategy="afterInteractive">
@@ -189,7 +189,7 @@ export default function App({
             function gtag(){window.dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', '${process.env.NEXT_GA_MEASUREMENT_ID}');
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
           `}
           </Script>
         </>
