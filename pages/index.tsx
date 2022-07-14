b2343ab1a2296a108c3dc72e8c770661594689e4
starlesssss <61955474+plrs9816@users.noreply.github.com>
2022-07-14 21:30:27 +0900

    feat: mobile search suggest

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import { Flex, Section } from "common/atoms";
+import { Button, Flex, Section } from "common/atoms";
 import { GetServerSideProps } from "next";
 import { dehydrate, QueryClient } from "react-query";
 import { createPageLayout } from "components";
-import { breakpoints, handleError } from "lib/utils";
+import { breakpoints, handleError, Media } from "lib/utils";
 import { CustomNextPage } from "types";
 import {
   BannerView,
@@ -13,12 +13,30 @@ import {
 } from "views/home";
 import { frontFeaturedQueryContext } from "lib/queries";
 import { css } from "@emotion/react";
+import Link from "next/link";
+import { MdOutlineSearch } from "react-icons/md";
 
 const Home: CustomNextPage = () => {
   return (
     <>
       <BannerView />
       <Section bleed css={{ borderTop: "0px" }}>
+        <Media at="sm">
+          <Link passHref href="/projects">
+            <Button
+              variant="primary-outlined"
+              as="a"
+              css={{
+                marginBottom: "24px",
+                justifyContent: "space-between",
+                marginTop: "-8px",
+              }}
+            >
+              <span css={{ fontSize: "1.4rem" }}>프로젝트 찾아보기</span>
+              <MdOutlineSearch />
+            </Button>
+          </Link>
+        </Media>
         <Flex css={style.flexWrapper}>
           <HotFeaturedView />
           <NewFeaturedView />
@@ -43,7 +61,7 @@ export default Home;
 const style = {
   flexWrapper: css({
     columnGap: "64px",
-    rowGap: "32px",
+    rowGap: "24px",
     flexWrap: "wrap",
     "& > *": {
       [breakpoints.greaterThan("sm")]: { overflow: "hidden" },
